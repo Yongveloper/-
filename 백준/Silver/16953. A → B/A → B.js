6e7b4316bdc1a4a8bca811f9baeac7f6fb3200c5
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-04-29 13:04:04 +0900

    [Silver II] Title: A → B, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(file).toString().trim().split(' ');
+const input = fs.readFileSync(file).toString().trim().split(' ').map(Number);
 
 let [num1, num2] = input;
 let answer = 1;
 
-while (+num1 < +num2) {
-  num2 = String(num2);
-  if (num2[num2.length - 1] == 1) {
-    num2 = num2.substring(0, num2.length - 1);
+while (num1 < num2) {
+  if (num2 % 10 === 1) {
+    num2 = parseInt(num2 / 10, 10);
   } else {
     num2 /= 2;
   }
