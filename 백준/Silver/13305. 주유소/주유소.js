1b74f638e67ff063b7d0302e997921b81525f897
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-05-04 02:29:02 +0900

    [Silver III] Title: 주유소, Time: 276 ms, Memory: 34268 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+
+const distances = input[1].split(' ');
+const prices = input[2].split(' ');
+const arr = distances.map((item, i) => [+prices[i], +item]);
+
+let min = 1000000001;
+let sum = 0;
+
+for (const [price, distance] of arr) {
+  if (price <= min) {
+    sum += price * distance;
+    min = price;
+  } else {
+    sum += min * distance;
+  }
+}
+
+console.log(sum);
