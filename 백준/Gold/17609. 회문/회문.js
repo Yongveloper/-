4f118c401d63da16e3dae517832feb42abfc54c5
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-05-12 15:57:09 +0900

    [Gold V] Title: 회문, Time: 932 ms, Memory: 284828 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.js
+++ 백준/Gold/17609. 회문/회문.js
@@ -4,31 +4,31 @@ const input = fs.readFileSync(file).toString().trim().split('\n');
 
 const [t, ...arr] = input;
 
-function palindrome(s) {
-  return s === s.split('').reverse().join('');
+function palindrome(str) {
+  return str === str.split('').reverse().join('');
 }
+
+let answer = '';
 
 for (const str of arr) {
   if (palindrome(str)) {
-    console.log(0); //회문인 경우
+    answer += '0\n';
   } else {
-    // 회문이 아닌 경우, 유사 회문인지 검사
     let found = false;
-    let n = str.length; // 문자열 길이
+    let n = str.length;
     for (let i = 0; i < Math.floor(n / 2); i++) {
       if (str[i] !== str[n - i - 1]) {
-        // 대칭이 아닌 인덱스를 찾은 경우
-        // 앞쪽에 있는 해당 원소를 제거해 본 뒤에 회문 검사
         if (palindrome(str.slice(0, i) + str.slice(i + 1, n))) {
           found = true;
         }
-        // 뒤쪽에 있는 해당 원소를 제거해 본 뒤에 회문 검사
         if (palindrome(str.slice(0, n - i - 1) + str.slice(n - i, n))) {
           found = true;
         }
         break;
       }
     }
-    found ? console.log(1) : console.log(2);
+    found ? (answer += '1\n') : (answer += '2\n');
   }
 }
+
+console.log(answer);
