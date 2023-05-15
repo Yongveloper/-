d5b4f2a9849681a79e9d9cf865c3cc3c69bb7c7e
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-05-16 01:36:40 +0900

    [Silver III] Title: 예산, Time: 188 ms, Memory: 11972 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -5,25 +5,21 @@ const input = fs.readFileSync(file).toString().trim().split('\n');
 const arr = input[1].split(' ').map(Number);
 const m = Number(input[2]);
 
-let start = 1; // 이진 탐색을 위한 시작점(start)과 끝점(end) 설정
+let start = 1;
 let end = Math.max(...arr);
 
 let result = 0;
 
 while (start <= end) {
-  // 이진 탐색 수행(반복문)
-  const mid = Math.floor((start + end) / 2); // 현재의 중간점(상한액)
-  let total = 0; // 배정된 예산의 총액 계산
-  for (x of arr) {
-    // 각 지방에서 요쳥한 예산을 하나씩 확인하며
+  const mid = Math.floor((start + end) / 2);
+  let total = 0;
+  for (const x of arr) {
     total += Math.min(x, mid);
   }
   if (total <= m) {
-    // 조건을 만족한다면, 상한액(최대화가 목표)을 증가시키기
     result = mid;
     start = mid + 1;
   } else {
-    // 조건을 만족하지 못한다면, 상한액을 감소시키기
     end = mid - 1;
   }
 }
