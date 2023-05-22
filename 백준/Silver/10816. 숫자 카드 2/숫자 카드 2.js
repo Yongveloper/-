6e37c57883b311c41bf7409c47d70b70509450b7
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-05-22 14:44:36 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 900 ms, Memory: 165436 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -3,46 +3,16 @@ const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().trim().split('\n');
 
 const arr1 = input[1].split(' ').map(Number);
-arr1.sort((a, b) => a - b);
 const arr2 = input[3].split(' ').map(Number);
+const map = new Map();
 let answer = '';
 
-function lowerBound(arr, target, start, end) {
-  while (start < end) {
-    const mid = Math.floor((start + end) / 2);
-    if (arr[mid] >= target) {
-      end = mid; // 최대한 왼쪽으로 이동하기
-    } else {
-      start = mid + 1;
-    }
-  }
-  return end;
-}
-
-// 정렬된 순서를 유지하면서 배열에 삽입할 가장 오른쪽 인덱스를 반환
-function upperBound(arr, target, start, end) {
-  while (start < end) {
-    const mid = Math.floor((start + end) / 2);
-    if (arr[mid] > target) {
-      end = mid;
-    } else {
-      start = mid + 1; // 최대한 오른쪽으로 이동하기
-    }
-  }
-  return end;
-}
-
-// 값이 [leftValue, rightValue]인 데이터의 개수를 반환하는 함수
-function countByRange(arr, leftValue, rightValue) {
-  // 유의: lowerBound와 upperBound는 end 변수의 값을 배열의 길이로 설정
-  const rightIndex = upperBound(arr, rightValue, 0, arr.length);
-  const leftIndex = lowerBound(arr, leftValue, 0, arr.length);
-
-  return rightIndex - leftIndex;
+for (const x of arr1) {
+  map.set(x, map.get(x) + 1 || 1);
 }
 
 for (const x of arr2) {
-  answer += countByRange(arr1, x, x) + ' ';
+  answer += `${map.get(x) || 0} `;
 }
 
 console.log(answer);
