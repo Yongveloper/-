99b79b66529f5132eaa3c22f9f14c77e9278f9be
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-05-30 00:43:33 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 17408 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().trim().split(' ').map(Number);
-const [n, m] = input;
-const array = new Array(m).fill(0);
-const check = new Array(n).fill(false);
-let answer = '';
 
-function dfs(L) {
-  if (L === m) {
-    answer += `${array.join(' ')}\n`;
+const [n, m] = input;
+let answer = '';
+const arr = [];
+const visited = new Array(n).fill(false);
+
+function dfs(depth) {
+  if (depth === m) {
+    answer += arr.join(' ') + '\n';
     return;
   }
   for (let i = 1; i <= n; i++) {
-    if (check[i]) continue;
-    array[L] = i;
-    check[i] = true;
-    dfs(L + 1);
-    check[i] = false;
+    if (visited[i]) continue;
+    visited[i] = true;
+    arr.push(i);
+    dfs(depth + 1);
+    visited[i] = false;
+    arr.pop(i);
   }
 }
+
 dfs(0);
 
 console.log(answer);
