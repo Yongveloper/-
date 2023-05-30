d2b54d8dccd26855fbd9f234f41c5ebbfd998366
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-05-31 02:31:50 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 9384 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -1,20 +1,29 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().trim().split(' ').map(Number);
-const [n, m] = input;
-const array = Array.from({ length: m }, () => 0);
-let answer = '';
 
-function dfs(L, start) {
-  if (L === m) {
-    answer += `${array.join(' ')}\n`;
+const [n, m] = input;
+const arr = Array.from({ length: n }, (_, i) => i + 1);
+const selected = [];
+
+let answer = '';
+function dfs(depth, start) {
+  if (depth === m) {
+    const result = [];
+    for (const i of selected) {
+      result.push(arr[i]);
+    }
+    for (const x of result) {
+      answer += x + ' ';
+    }
+    answer += '\n';
     return;
   }
-  for (let i = start; i <= n; i++) {
-    array[L] = i;
-    dfs(L + 1, i + 1);
+  for (let i = start; i < n; i++) {
+    selected.push(i);
+    dfs(depth + 1, i + 1);
+    selected.pop();
   }
 }
-dfs(0, 1);
-
-console.log(answer.trim());
+dfs(0, 0);
+console.log(answer);
