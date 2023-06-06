f7cb47991801272d1a0b3d903deb0f48c413a71b
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-06-07 02:14:42 +0900

    [Silver II] Title: 외판원 순회 2, Time: 220 ms, Memory: 10092 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.js
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.js
@@ -3,26 +3,26 @@ const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const l = input.slice(1).map((line) => line.split(' ').map(Number));
+const graph = input.slice(1).map((line) => line.split(' ').map(Number));
 
-const visit = new Array(n).fill(0);
+const visited = new Array(n).fill(false);
 let m = 1e9;
 
 function dfs(depth, start, cost) {
-  if (depth === n - 1 && l[start][0] !== 0) {
-    m = Math.min(m, cost + l[start][0]);
+  if (depth === n - 1 && graph[start][0] !== 0) {
+    m = Math.min(m, cost + graph[start][0]);
     return;
   }
   for (let i = 0; i < n; i++) {
-    if (!visit[i] && l[start][i] !== 0) {
-      visit[i] = 1;
-      dfs(depth + 1, i, cost + l[start][i]);
-      visit[i] = 0;
+    if (!visited[i] && graph[start][i] !== 0) {
+      visited[i] = true;
+      dfs(depth + 1, i, cost + graph[start][i]);
+      visited[i] = false;
     }
   }
 }
 
-visit[0] = 1;
+visited[0] = true;
 dfs(0, 0, 0);
 
 console.log(m);
