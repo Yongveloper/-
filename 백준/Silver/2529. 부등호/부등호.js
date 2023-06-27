6aada28c8939628d9df2093a39e40b4bee7668f5
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-06-28 01:44:37 +0900

    [Silver I] Title: 부등호, Time: 676 ms, Memory: 12620 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+
+const k = Number(input[0]);
+const arr = input[1].split(' ');
+const visited = new Array(10).fill(false);
+let result = [];
+let current = '';
+let first = '';
+
+function dfs(depth) {
+  if (depth === k + 1) {
+    let check = true;
+    for (let i = 0; i < k; i++) {
+      if (arr[i] === '<') {
+        if (result[i] > result[i + 1]) check = false;
+      } else if (arr[i] === '>') {
+        if (result[i] < result[i + 1]) check = false;
+      }
+    }
+    if (check) {
+      current = '';
+      for (const x of result) {
+        current += x + '';
+      }
+      if (first === '') {
+        first = current;
+      }
+    }
+    return;
+  }
+
+  for (let i = 0; i < 10; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    result.push(i);
+    dfs(depth + 1);
+    visited[i] = false;
+    result.pop();
+  }
+}
+
+dfs(0);
+console.log(current + '\n' + first);
