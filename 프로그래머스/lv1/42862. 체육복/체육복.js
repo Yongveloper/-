da0f5ae74c6ae32dc548d00e0eb0109eac7a8986
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-07-08 00:14:11 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,18 @@
+function solution(n, lost, reserve) {
+    let answer = n - lost.length;
+    const realLost = lost.sort((a,b) => a  - b).filter(item => !reserve.includes(item));
+    answer += lost.length - realLost.length;
+    const realReserve = reserve.sort((a,b) => a-b).filter(item => !lost.includes(item));
+
+    for(let i = 0; i < realLost.length; i++) {
+        for(let j = 0; j < realReserve.length; j++){
+            if(Math.abs(realLost[i] - realReserve[j]) === 1 || realLost[i] === realReserve[j]) {
+                answer++;
+                realReserve.splice(j, 1);
+                break;
+            } 
+        }
+    }
+
+    return answer;
+}
