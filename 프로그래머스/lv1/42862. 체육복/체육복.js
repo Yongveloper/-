3bdf926dc111432e6a30247fd0a87dcb325dca1d
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-07-08 00:21:25 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -6,7 +6,7 @@ function solution(n, lost, reserve) {
 
     for(let i = 0; i < realLost.length; i++) {
         for(let j = 0; j < realReserve.length; j++){
-            if(Math.abs(realLost[i] - realReserve[j]) === 1 || realLost[i] === realReserve[j]) {
+            if(Math.abs(realLost[i] - realReserve[j]) === 1) {
                 answer++;
                 realReserve.splice(j, 1);
                 break;
