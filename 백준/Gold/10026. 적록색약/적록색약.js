978d6690a9899dac2fb9465599f219f7935a40cb
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-07-15 18:42:16 +0900

    [Gold V] Title: 적록색약, Time: 188 ms, Memory: 10760 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.js
+++ 백준/Gold/10026. 적록색약/적록색약.js
@@ -4,35 +4,21 @@ const input = fs.readFileSync(file).toString().trim().split('\n');
 
 const n = Number(input[0]);
 const graph = input.slice(1).map((item) => item.split(''));
-const visited = [];
-for (const x of graph) {
-  visited.push([...x]);
-}
+const visited = Array.from({ length: n }, () => new Array(n).fill(false));
 
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
-const result = {
-  R: 0,
-  G: 0,
-  B: 0,
-};
 
-const dfs = (x, y, target) => {
-  if (x < 0 || x >= n || y < 0 || y >= n) {
-    return false;
-  }
-  if (visited[x][y] === 'A') {
-    return false;
-  }
-  if (graph[x][y] !== target) {
-    return false;
-  }
-
-  visited[x][y] = 'A';
+const dfs = (x, y) => {
+  if (visited[x][y]) return false;
+  visited[x][y] = true;
   for (let i = 0; i < 4; i++) {
     const nx = x + dx[i];
     const ny = y + dy[i];
-    dfs(nx, ny, target);
+    if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+    if (graph[x][y] === graph[nx][ny]) {
+      dfs(nx, ny);
+    }
   }
   return true;
 };
@@ -40,7 +26,7 @@ const dfs = (x, y, target) => {
 let answer = 0;
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
-    if (dfs(i, j, graph[i][j])) {
+    if (dfs(i, j)) {
       answer++;
     }
   }
@@ -48,7 +34,7 @@ for (let i = 0; i < n; i++) {
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
-    visited[i][j] = graph[i][j];
+    visited[i][j] = false;
     if (graph[i][j] === 'R') {
       graph[i][j] = 'G';
     }
@@ -58,7 +44,7 @@ for (let i = 0; i < n; i++) {
 let answer2 = 0;
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
-    if (dfs(i, j, graph[i][j])) {
+    if (dfs(i, j)) {
       answer2++;
     }
   }
