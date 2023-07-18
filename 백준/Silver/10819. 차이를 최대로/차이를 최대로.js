273fda470df679522e4a3d7af214e17005f80810
yongveloper <64254228+Yongveloper@users.noreply.github.com>
2023-07-18 21:11:09 +0900

    [Silver II] Title: 차이를 최대로, Time: 200 ms, Memory: 10356 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -6,27 +6,27 @@ const n = Number(input[0]);
 const arr = input[1].split(' ').map(Number);
 const visited = new Array(n).fill(false);
 const result = [];
-let max = -Infinity;
+let answer = 0;
 
-function dfs(depth) {
+const dfs = (depth) => {
   if (depth === n) {
     let sum = 0;
     for (let i = 0; i < n - 1; i++) {
       sum += Math.abs(result[i] - result[i + 1]);
     }
-    max = Math.max(max, sum);
+    answer = Math.max(answer, sum);
     return;
   }
 
   for (let i = 0; i < n; i++) {
     if (visited[i]) continue;
-    visited[i] = true;
     result.push(arr[i]);
+    visited[i] = true;
     dfs(depth + 1);
-    visited[i] = false;
     result.pop();
+    visited[i] = false;
   }
-}
+};
 
 dfs(0);
-console.log(max);
+console.log(answer);
