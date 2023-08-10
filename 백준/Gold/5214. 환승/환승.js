e57f6cb4050adfebe5fa2b7a0a0accf2e41df581
Hyun Jun Yong <64254228+Yongveloper@users.noreply.github.com>
2023-08-10 15:19:23 +0900

    [Gold II] Title: 환승, Time: 1064 ms, Memory: 100524 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.js
+++ 백준/Gold/5214. 환승/환승.js
@@ -30,26 +30,23 @@ class Queue {
   }
 }
 
-// 역의 개수(N), 간선의 개수(K), 하이퍼튜브의 개수(M)
 const [n, k, m] = input[0].split(' ').map(Number);
-// 그래프 정보(N개의 역과 M개의 하이퍼튜브는 모두 노드)
 const graph = Array.from({ length: n + m + 1 }, () => []);
 for (let i = 1; i <= m; i++) {
   const arr = input[i].split(' ').map(Number);
   for (const x of arr) {
-    graph[x].push(n + i); // 노드 -> 하이퍼 튜브
-    graph[n + i].push(x); // 하이퍼 튜브 -> 노드
+    graph[x].push(n + i);
+    graph[n + i].push(x);
   }
 }
-const visited = new Set([1]); // 1번 노드에서 출발
+const visited = new Set([1]);
 const queue = new Queue();
-queue.enqueue([1, 1]); // [거리, 노드 번호]
+queue.enqueue([1, 1]);
 
 while (queue.size() !== 0) {
   const [dist, now] = queue.dequeue();
-  // N번 노드에 도착한 경우
+
   if (now === n) {
-    // 절반은 하이퍼 튜브
     console.log(Math.floor(dist / 2) + 1);
     return;
   }
