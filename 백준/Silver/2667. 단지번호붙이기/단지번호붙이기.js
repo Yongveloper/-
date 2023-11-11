0f8f4ba9f37212bbc945b5f1a2d202643a7aa9bb
Hyun Jun Yong <64254228+Yongveloper@users.noreply.github.com>
2023-11-11 15:26:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -3,41 +3,43 @@ const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const graph = input.slice(1).map((item) => item.split('').map(Number));
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
+const arr = input.slice(1).map((item) => item.split('').map(Number));
+const result = [];
+const dx = [0, 1, 0, -1];
+const dy = [-1, 0, 1, 0];
 let count = 0;
+
 const dfs = (x, y) => {
   if (x < 0 || x >= n || y < 0 || y >= n) {
     return false;
   }
 
-  if (graph[x][y] === 0) {
+  if (arr[x][y] === 0) {
     return false;
   }
 
-  graph[x][y] = 0;
+  arr[x][y] = 0;
   count++;
 
   for (let i = 0; i < 4; i++) {
     const nx = x + dx[i];
     const ny = y + dy[i];
+
     dfs(nx, ny);
   }
 
   return true;
 };
 
-let connected = 0;
-const counts = [];
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
     if (dfs(i, j)) {
-      connected++;
-      counts.push(count);
+      result.push(count);
       count = 0;
     }
   }
 }
 
-console.log(connected + '\n' + counts.sort((a, b) => a - b).join('\n'));
+result.sort((a, b) => a - b);
+
+console.log(result.length + '\n' + result.join('\n'));
