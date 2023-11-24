726233a2d942b5b96124c5ee7f0e09329bf6349c
Hyun Jun Yong <64254228+Yongveloper@users.noreply.github.com>
2023-11-25 01:17:34 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -2,7 +2,6 @@ function solution(fees, records) {
   const [pt, pp, m, p] = fees;
   const arr = records.map((item) => item.split(' '));
   arr.sort((a, b) => +a[1] - +b[1]);
-  console.log(arr);
   const map = new Map();
   let prevCar = arr[0];
 
