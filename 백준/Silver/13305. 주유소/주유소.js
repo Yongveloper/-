fd5a0032d41b75159223704b8c286cbccbe658b0
Hyun Jun Yong <64254228+Yongveloper@users.noreply.github.com>
2024-03-11 23:13:48 +0900

    [Silver III] Title: 주유소, Time: 352 ms, Memory: 50444 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,39 +2,20 @@ const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().trim().split('\n');
 
-// 내 풀이
 const distances = input[1].split(' ');
 const prices = input[2].split(' ');
-const arr = distances.map((item, i) => [BigInt(prices[i]), BigInt(item)]);
+const arr = distances.map((item, i) => [+prices[i], +item]);
 
 let min = Infinity;
 let sum = BigInt(0);
 
 for (const [price, distance] of arr) {
   if (price <= min) {
-    sum += price * distance;
+    sum += BigInt(price) * BigInt(distance);
     min = price;
   } else {
-    sum += BigInt(min) * distance;
+    sum += BigInt(min) * BigInt(distance);
   }
 }
 
-console.log(sum.toString());
-
-// 나동빈 풀이
-// let n = Number(input[0]);
-// let km = input[1].split(' ').map(Number);
-// let cost = input[2].split(' ').map(Number);
-
-// //비오름차순이 되도록 정렬
-// let minCost = cost[0];
-// for (let i = 0; i < n; i++) {
-//   minCost = Math.min(minCost, cost[i]);
-//   cost[i] = minCost;
-// }
-
-// let answer = BigInt(0);
-// for (let i = 0; i < n - 1; i++) {
-//   answer += BigInt(km[i]) * BigInt(cost[i]);
-// }
-// console.log(String(answer));
+console.log(String(sum));
