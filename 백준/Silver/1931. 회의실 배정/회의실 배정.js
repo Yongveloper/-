ba510edd8a48f222b82ee9a58acecf1198cf4140
Hyun Jun Yong <64254228+Yongveloper@users.noreply.github.com>
2024-03-12 16:03:51 +0900

    [Silver I] Title: 회의실 배정, Time: 392 ms, Memory: 43656 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -2,24 +2,22 @@ const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(file).toString().trim().split('\n');
 
-const [n, ...arr] = input;
-let answer = 0;
-const times = arr
-  .map((num) => num.split(' ').map((num) => +num))
-  .sort((a, b) => {
-    if (a[1] === b[1]) {
-      return a[0] - b[0];
-    } else {
-      return a[1] - b[1];
-    }
-  });
+const arr = input.slice(1).map((item) => item.split(' ').map(Number));
 
-let et = 0;
-times.forEach((time) => {
-  if (time[0] >= et) {
-    answer++;
-    et = time[1];
+arr.sort((a, b) => {
+  if (a[1] === b[1]) {
+    return a[0] - b[0];
+  } else {
+    return a[1] - b[1];
   }
 });
 
-console.log(answer);
+const result = [arr[0]];
+
+for (let i = 1; i < arr.length; i++) {
+  if (result.at(-1)[1] <= arr[i][0]) {
+    result.push(arr[i]);
+  }
+}
+
+console.log(result.length);
