f300d93b343c53174bddcaf51a4749e93f613473
Yong <aaa9015@naver.com>
2024-08-06 22:03:04 +0900

    [level 2] Title: 짝지어 제거하기, Time: 99.25 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -3,7 +3,7 @@ function solution(s) {
     // 2. 두번째 요소부터 스택에 넣고 이전 요소와 비교한다.
         // 만약 같은 요소라면 넣지 않고, 제거를 해준다.
     // 위 과정을 반복한다.
-    if(s.length === 0) {
+    if(s.length === 1) {
         return 0;
     }
     const stack = [s[0]];
